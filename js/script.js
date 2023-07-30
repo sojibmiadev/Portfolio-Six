9de13c329b1229ce9e1f2017a613c39ba5f99849
Md. Sojib Mia <140948644+sojibmiadev@users.noreply.github.com>
2023-07-30 18:07:24 +0600

    Portfolio Project

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// scroll section active link 
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    // sticky navbar start
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove toggle icon and navbar when click scroll bar 
+    menuIcon.classList.remove('fa-x');
+    navbar.classList.remove('active');
+
+};
+
+// toggle icon navbar start 
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-x');
+    navbar.classList.toggle('active');
+};
+
+// typed js 
+const typed = new Typed('.multiple-text', {
+    strings: ['frontend developer!', 'digital marketor!', 'graphics designer!'],
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 1000,
+      loop: true,
+});
+
